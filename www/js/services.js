c5c70af956b445f8b5ff8cbc814b771f2e21191c
Georgi Knox <georgiknox@gmail.com>
2014-01-17 15:29:36 +1100

    Removed unused attributes on program model

--- www/js/services.js
+++ www/js/services.js
@@ -21,20 +21,20 @@ angular.module('sheHacksApp.services', ['ngResource', 'sheHacksApp.configuration
     .factory('ProgramService', function () {
         var programDays = [
             {"date": "FRIDAY 21st MARCH, 2014", "program": [
-                {"title": "Guest Arrival Registration", "time": "18:00", iconClass: '', colour: "candy-"},
-                {"title": "Welcome & Lightening Talks", "time": "18:30", iconClass: '', colour: "candy-" },
-                {"title": "Pitches & Group Formation", "time": "19:00", iconClass: '', colour: "candy-"},
-                {"title": "Dinner", "time": "20:00", iconClass: '', colour: "candy-"},
-                {"title": "Start Hacking", "time": "20:00-<br>24:00", iconClass: '', colour: "candy-"}
+                {"title": "Guest Arrival & Registration", "time": "18:00"},
+                {"title": "Welcome & Lightening Talks", "time": "18:30"},
+                {"title": "Pitches & Group Formation", "time": "19:00"},
+                {"title": "Dinner", "time": "20:00", iconClass: ''},
+                {"title": "Start Hacking", "time": "20:00-<br>24:00"}
             ]},
             {"date": "SATURDAY 22nd MARCH, 2014", "program": [
-                {"title": "Coffee & Hacking", "time": "09:00", iconClass: '', colour: "candy-"},
-                {"title": "Morning Tea", "time": "10:00", iconClass: '', colour: "candy-"},
-                {"title": "Lunch", "time": "12:00", iconClass: '', colour: "candy-"},
-                {"title": "30 Minute Warning Till Pitch Time", "time": "16:00", iconClass: '', colour: "candy-"},
-                {"title": "Project Pitches", "time": "16:30", iconClass: '', colour: "candy-"},
-                {"title": "Judging and Prizes", "time": "15:30", iconClass: '', colour: "candy-"},
-                {"title": "Party Time", "time": "18:00-<br>Late", iconClass: '', colour: "candy-"}
+                {"title": "Coffee & Hacking", "time": "09:00"},
+                {"title": "Morning Tea", "time": "10:00"},
+                {"title": "Lunch", "time": "12:00"},
+                {"title": "30 Minute Warning Till Pitches", "time": "16:00"},
+                {"title": "Team Pitches", "time": "16:30"},
+                {"title": "Judging and Prizes", "time": "15:30"},
+                {"title": "Party Time", "time": "18:00-<br>Late"}
             ]}
         ];
 
@@ -55,7 +55,7 @@ angular.module('sheHacksApp.services', ['ngResource', 'sheHacksApp.configuration
     .factory('PrizesService', function () {
         var prizes = [
             { name: 'Best Use of Search', desc: 'sponsored by Google'},
-            { name: 'Best Use of Social Media', desc: 'sponsored by Mi9'},
+            { name: 'Best Use of Social Media', desc: 'sponsored by Microsoft'},
             { name: 'Peoples Choice', desc: 'as voted by audience'}
         ];
 
